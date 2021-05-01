f323fddcda94faaf98db6cbc62e89e124eea0c33
PeterSalnikov <psalniko@sfu.ca>
2021-05-01 14:47:51 -0700

    tracks now saved as list

--- spotify.py
+++ spotify.py
@@ -6,27 +6,22 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 
 scope = "user-library-read"
-#
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 target = sp.playlist_items(playlist_id='4gKMgvqgoB3j09i3J4Akhx?si=655b7f9d1c324f3f', fields='tracks,next')
 
 
-def show_tracks(results):
+def save_tracks(results):
+    tracks_artists = []
     for i, item in enumerate(results['items']):
         track = item['track']
-        print(
-            "   %d %32.32s %s" %
-            (i, track['artists'][0]['name'], track['name']))
+        tracks_artists.append(track['artists'][0]['name'] + " " + track['name'])
+    return tracks_artists
 
 
 tracks = target['tracks']
 
-# print(target['name'])
+list = save_tracks(tracks)
+print(list)
 
-show_tracks(tracks)
-
-# print(target['tracks']['name'])
-# df = pd.DataFrame(target)
-
-# print(df.to_string())
