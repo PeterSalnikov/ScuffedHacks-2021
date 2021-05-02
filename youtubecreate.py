18a21994e61f1b0bc962ad2bced0283410d2d4c4
PeterSalnikov <psalniko@sfu.ca>
2021-05-01 18:30:40 -0700

    Working save for exceeding quota limit

--- youtubecreate.py
+++ youtubecreate.py
@@ -5,32 +5,32 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
-
+import time
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 
-def get_client():
-    """ Taken from Youtube's Data API """
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+# def get_client():
+""" Taken from Youtube's Data API """
+# Disable OAuthlib's HTTPS verification when running locally.
+# *DO NOT* leave this option enabled in production.
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret.json"
+api_service_name = "youtube"
+api_version = "v3"
+client_secrets_file = "client_secret.json"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    client = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-    return client
+# Get credentials and create an API client
+flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+    client_secrets_file, scopes)
+credentials = flow.run_console()
+client = googleapiclient.discovery.build(
+    api_service_name, api_version, credentials=credentials)
+    # return client
 
 def yt_search(playlist_id,title_artist):
     """Searches for video using song title and artist and gets first result"""
-    request = get_client().search().list(
+    request = client.search().list(
         part="snippet",
         maxResults=1,
         q=title_artist
@@ -43,7 +43,7 @@ def yt_search(playlist_id,title_artist):
 
 def playlist_input(playlistid, videoid):
     """Adds to youtube playlist"""
-    request = get_client().playlistItems().insert(
+    request = client.playlistItems().insert(
         part="snippet",
         body={
             "snippet": {
@@ -67,3 +67,4 @@ if __name__ == "__main__":
 
     for index, row in df.iterrows():
         yt_search(playlist_id, row['tracks'])
+        time.sleep(0.5)
